0312816baa15adf19efb674e79c3ef49b3641823
Matthew Jimoya <145257025+Technofusion10@users.noreply.github.com>
2026-06-11 08:45:55 +0800

    commit

---
+++ style.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const slider = document.getElementById("slider");
+    const items = document.querySelectorAll(".nav-item");
+
+    function moveSlider(item) {
+        slider.style.width = item.offsetWidth + "px";
+        slider.style.height = item.offsetHeight + "px";
+        slider.style.left = item.offsetLeft + "px";
+        slider.style.top = item.offsetTop + "px";
+
+        items.forEach(link => {
+            link.classList.remove("text-white");
+            link.classList.add("text-gray-700");
+        });
+
+        item.classList.remove("text-gray-700");
+        item.classList.add("text-white");
+    }
+
+    // Set initial position
+    moveSlider(items[0]);
+
+    items.forEach(item => {
+
+        item.addEventListener("mouseenter", () => {
+            moveSlider(item);
+        });
+
+    });
+
+});
